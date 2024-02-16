c93886811e92020f3f1f11a92579b52fe9dd8b8f
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-02-16 15:46:53 -0300

    🐛 Fix navbar

--- Eduardo-Barreto/docusaurus.config.ts
+++ Eduardo-Barreto/docusaurus.config.ts
@@ -51,25 +51,11 @@ const config: Config = {
         // Replace with your project's social card
         image: 'img/docusaurus-social-card.jpg',
         navbar: {
-            title: 'My Site',
+            title: 'Eduardo Barreto',
             logo: {
                 alt: 'My Site Logo',
                 src: 'img/logo.svg',
             },
-            items: [
-                {
-                    type: 'docSidebar',
-                    sidebarId: 'tutorialSidebar',
-                    position: 'left',
-                    label: 'Tutorial',
-                },
-                { to: '/blog', label: 'Blog', position: 'left' },
-                {
-                    href: 'https://github.com/facebook/docusaurus',
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
         },
         footer: {
             style: 'dark',
